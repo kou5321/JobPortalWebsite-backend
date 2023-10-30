4f0bda0a4535a4f136aed16314679a14e7d3dfd0
Kou <ykou@itc-imac-15.ur.rochester.edu>
2023-10-29 20:34:25 -0400

    update databse connection and change the dependecy to springdoc

--- src/main/java/com/kou5321/jobPortalWebsite/JobPortalWebsiteApplication.java
+++ src/main/java/com/kou5321/jobPortalWebsite/JobPortalWebsiteApplication.java
@@ -2,8 +2,9 @@ package com.kou5321.jobPortalWebsite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class JobPortalWebsiteApplication {
 
 	public static void main(String[] args) {
