bed4cf3beb6afea98813ffe6d7accd739c073c26
Kou <ykou@itc-imac-15.ur.rochester.edu>
2023-10-29 20:46:57 -0400

    redirect main page to springdoc

--- src/main/java/com/kou5321/jobPortalWebsite/PostController.java
+++ src/main/java/com/kou5321/jobPortalWebsite/PostController.java
@@ -1,7 +1,18 @@
 package com.kou5321.jobPortalWebsite;
 
+import io.swagger.v3.oas.annotations.Hidden;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
+
 public class PostController {
+    @RequestMapping("/")
+    @Hidden
+    public void redirect(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/swagger-ui.html");
+    }
 }
