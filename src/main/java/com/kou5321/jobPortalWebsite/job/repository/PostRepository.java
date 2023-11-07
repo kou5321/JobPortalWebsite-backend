35541504bbc54d253f320bb87399f5f85f66de9c
Yixiao KOU <43342143+kou5321@users.noreply.github.com>
2023-11-07 15:38:26 -0500

    delete redundant file

--- src/main/java/com/kou5321/jobPortalWebsite/job/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kou5321.jobPortalWebsite.job.repository;
-
-import com.kou5321.jobPortalWebsite.job.model.Post;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PostRepository extends MongoRepository<Post, String> {
-}
--- src/main/java/com/kou5321/jobPortalWebsite/job/repository/SearchRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kou5321.jobPortalWebsite.job.repository;
-
-import com.kou5321.jobPortalWebsite.job.model.Post;
-
-import java.util.List;
-
-public interface SearchRepository {
-    List<Post> findByText(String text);
-}
