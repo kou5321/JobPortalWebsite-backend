95a20a69656ad1fd8b4a36005f6071e56a45f23f
Kou <ykou@itc-imac-25.ur.rochester.edu>
2023-11-22 18:47:33 -0500

    add pagination for getAllJobPost

--- src/main/java/com/kou5321/jobPortalWebsite/job/controller/JobPostingController.java
+++ src/main/java/com/kou5321/jobPortalWebsite/job/controller/JobPostingController.java
@@ -8,8 +8,11 @@ import io.swagger.v3.oas.annotations.Hidden;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.io.IOException;
 import java.util.List;
@@ -34,8 +37,11 @@ public class JobPostingController {
     }
 
     @GetMapping("/getAllJobPosts")
-    public List<JobPosting> getAllPosts() {
-        return jobPostingRepository.findAll();
+    public Page<JobPosting> getAllPosts(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "20") int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return jobPostingRepository.findAll(pageable);
     }
 
     @PostMapping("/addJobPost")
--- src/main/java/com/kou5321/jobPortalWebsite/job/repository/JobPostingRepository.java
+++ src/main/java/com/kou5321/jobPortalWebsite/job/repository/JobPostingRepository.java
@@ -1,7 +1,10 @@
 package com.kou5321.jobPortalWebsite.job.repository;
 
 import com.kou5321.jobPortalWebsite.job.model.JobPosting;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface JobPostingRepository extends MongoRepository<JobPosting, String> {
+    Page<JobPosting> findAll(Pageable pageable);
 }
