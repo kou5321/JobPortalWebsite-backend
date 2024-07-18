c19b0bd63d7624a0d684c3c5005ef9cd6cdff7df
kou5321 <757330477@qq.com>
2024-07-17 21:30:35 -0400

    change threadpool creation

--- src/main/java/com/kou5321/jobPortalWebsite/config/ThreadPoolConfig.java
+++ src/main/java/com/kou5321/jobPortalWebsite/config/ThreadPoolConfig.java
@@ -3,20 +3,35 @@ package com.kou5321.jobPortalWebsite.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.*;
 
 @Configuration
 public class ThreadPoolConfig {
 
-//    @Bean
-//    public ExecutorService taskExecutor() {
-//        return Executors.newFixedThreadPool(10); // Adjust the number of threads as needed
-//    }
-
     @Bean
     public ScheduledExecutorService scheduledExecutorService() {
-        return Executors.newScheduledThreadPool(10); // Adjust the number of threads as needed
+        int corePoolSize = 10;
+        ScheduledThreadPoolExecutor scheduledThreadPoolExecutor = new ScheduledThreadPoolExecutor(corePoolSize);
+
+        // Set the keep-alive time, thread factory, and other configurations if needed
+        scheduledThreadPoolExecutor.setKeepAliveTime(0L, TimeUnit.MILLISECONDS);
+        scheduledThreadPoolExecutor.setThreadFactory(Executors.defaultThreadFactory());
+
+        // Add a shutdown hook to properly shut down the executor
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            scheduledThreadPoolExecutor.shutdown();
+            try {
+                if (!scheduledThreadPoolExecutor.awaitTermination(60, TimeUnit.SECONDS)) {
+                    scheduledThreadPoolExecutor.shutdownNow();
+                }
+            } catch (InterruptedException e) {
+                scheduledThreadPoolExecutor.shutdownNow();
+                Thread.currentThread().interrupt();
+            }
+        }));
+
+        return scheduledThreadPoolExecutor;
     }
 }
