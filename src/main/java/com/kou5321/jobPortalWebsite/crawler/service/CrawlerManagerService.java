3858c7401e957cba525d9d061ef93f1f2fbc5c9c
kou5321 <757330477@qq.com>
2024-08-01 23:31:15 -0400

    add multithreading in crawler

--- src/main/java/com/kou5321/jobPortalWebsite/crawler/service/CrawlerManagerService.java
+++ src/main/java/com/kou5321/jobPortalWebsite/crawler/service/CrawlerManagerService.java
@@ -6,6 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
@@ -36,13 +38,32 @@ public class CrawlerManagerService {
             log.info("Deleting all existing job postings");
             jobPostingRepository.deleteAll();
 
+            // 创建一个线程池来并行执行爬虫任务
+            ExecutorService executorService = Executors.newFixedThreadPool(2);
+
             // 运行 GitHub 爬虫
-            log.info("Running GitHub Crawler");
-            githubCrawlerService.crawlGitHubJobPostings();
+            executorService.submit(() -> {
+                try {
+                    log.info("Running GitHub Crawler");
+                    githubCrawlerService.crawlGitHubJobPostings();
+                } catch (Exception e) {
+                    log.error("An error occurred while running GitHub Crawler: ", e);
+                }
+            });
 
             // 运行 JobPulse 爬虫
-            log.info("Running JobPulse Crawler");
-            jobPulseCrawlerService.fetchAndSaveJobPostings();
+            executorService.submit(() -> {
+                try {
+                    log.info("Running JobPulse Crawler");
+                    jobPulseCrawlerService.fetchAndSaveJobPostings();
+                } catch (Exception e) {
+                    log.error("An error occurred while running JobPulse Crawler: ", e);
+                }
+            });
+
+            // 关闭线程池
+            executorService.shutdown();
+            executorService.awaitTermination(1, TimeUnit.HOURS);
 
         } catch (Exception e) {
             log.error("An error occurred while running crawlers: ", e);
